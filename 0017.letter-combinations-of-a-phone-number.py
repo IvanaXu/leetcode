4e388b1ca87c10ecd3f5518e044b1c8943c12210
IvanaXu <1440420407@qq.com>
2023-01-27 03:21:42 +0800

    Create 0017.letter-combinations-of-a-phone-number.py

---
+++ 0017.letter-combinations-of-a-phone-number.py
@@ -0,0 +1,27 @@
+# https://leetcode.cn/problems/letter-combinations-of-a-phone-number/
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        r, n = [], 0
+        dic = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        for i in digits:
+            n += 1
+            d = dic[i]
+            if n == 1:
+                for j in d:
+                    r.append(j)
+            else:
+                r1 = []
+                for j in d:
+                    for k in r:
+                        r1.append(k+j)
+                r = r1
+        return r
