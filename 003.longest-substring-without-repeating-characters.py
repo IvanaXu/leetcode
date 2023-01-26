637d059878e821290b1209564600ade62a04e34b
IvanaXu <1440420407@qq.com>
2023-01-27 03:12:42 +0800

    Create 003.longest-substring-without-repeating-characters.py

---
+++ 003.longest-substring-without-repeating-characters.py
@@ -0,0 +1,14 @@
+# https://leetcode.cn/problems/longest-substring-without-repeating-characters/
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        j, m = "", 0
+        for i in s:
+            if i in j:
+                j = j[j.index(i)+1:]
+            j += i
+            m = max(m, len(j))
+        return m
