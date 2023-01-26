8e10499c7f7ca6dc277bae867a7e1f5d718aa3e0
IvanaXu <1440420407@qq.com>
2023-01-27 03:19:36 +0800

    Create 0011.container-with-most-water.py

---
+++ 0011.container-with-most-water.py
@@ -0,0 +1,24 @@
+# https://leetcode.cn/problems/container-with-most-water/
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # TODO: /
+        l1 = len(height)
+        l2 = l1 - 1
+        result,n = 0, 0
+
+        if height[0] > height[-1]:
+            height = height[::-1]
+
+        for i in range(l1):
+            for j in range(l1-n):
+                if l2-j-i > 0:
+                    rmin = min(height[i], height[l2-j])
+                    rcal = (l2-j-i) * rmin
+                    if rcal > result:
+                        result = rcal
+                    
+                    if rmin == height[i]:
+                        break
+                    if rmin == height[l2-j]:
+                        n += 1
+        return result
