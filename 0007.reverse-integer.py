1506ca9307e36e4bc3e34c1fc4a38b4a64c375be
IvanaXu <1440420407@qq.com>
2023-01-27 03:17:40 +0800

    Create 0007.reverse-integer.py

---
+++ 0007.reverse-integer.py
@@ -0,0 +1,10 @@
+# https://leetcode.cn/problems/reverse-integer/
+class Solution:
+    def reverse(self, x: int) -> int:
+        base = 1
+        if x < 0:
+            x = abs(x)
+            base = -1
+        r = base*int(str(x)[::-1])
+        return r if r >= -2147483648 and r <= 2147483647 else 0
+        
