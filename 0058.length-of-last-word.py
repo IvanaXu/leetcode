1b0727ddd2a215591a9cf78f8580b98a5666b202
IvanaXu <1440420407@qq.com>
2023-01-27 03:26:13 +0800

    Create 0058.length-of-last-word.py

---
+++ 0058.length-of-last-word.py
@@ -0,0 +1,9 @@
+# https://leetcode.cn/problems/length-of-last-word/
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        n = 0
+        for i in s.rstrip(" ")[::-1]:
+            if i == " ":
+                break
+            n += 1
+        return n
