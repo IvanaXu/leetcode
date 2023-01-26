36bc2d9f834ff0a21e41d0542fa5032461fdf97a
IvanaXu <1440420407@qq.com>
2023-01-27 03:20:31 +0800

    Create 0014.longest-common-prefix.py

---
+++ 0014.longest-common-prefix.py
@@ -0,0 +1,18 @@
+# https://leetcode.cn/problems/longest-common-prefix/
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        l, m, mk = "", 0, ""
+        for i in strs:
+            j = len(i)
+            if m == 0 or j <= m:
+                m = j
+                l = i
+        for k in range(0, m):
+            mk = l[:m-k]
+            r = set()
+            for i in strs:
+                r.add(i[:m-k])
+            if len(r) == 1:
+                break
+            mk = ""
+        return mk
