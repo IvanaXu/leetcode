8a0fa05a3d225c5a5410d6a1601a6831fd976cf7
IvanaXu <1440420407@qq.com>
2023-01-27 16:28:02 +0800

    Create 0374.guess-number-higher-or-lower.py

---
+++ 0374.guess-number-higher-or-lower.py
@@ -0,0 +1,22 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        import random
+        minn, maxn = 1, n
+
+        while True:
+            pick = random.randint(minn, maxn)
+            
+            new_r = guess(pick)
+            if new_r == 0:
+                return pick
+            elif new_r == -1:
+                maxn = pick
+            elif new_r == 1:
+                minn = pick
