0367e5b9889e70f77386621c911fc674151def35
IvanaXu <1440420407@qq.com>
2023-01-30 13:45:03 +0800

    Create|Update 0326.power-of-three.py

--- 0326.power-of-three.py
+++ 0326.power-of-three.py
@@ -1,3 +1,11 @@
 class Solution:
     def isPowerOfThree(self, n: int) -> bool:
-        return n > 0 and 1162261467 % n == 0
+        while True:
+            if n == 0:
+                return False
+            elif n == 1:
+                return True
+            elif n % 3 == 0:
+                n = n/3
+            else:
+                return False
