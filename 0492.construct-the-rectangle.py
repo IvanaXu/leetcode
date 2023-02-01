c78ab4495be853986c374c418f7f90e4de93f8bc
IvanaXu <1440420407@qq.com>
2023-02-02 01:55:04 +0800

    Create|Update 0492.construct-the-rectangle.py

---
+++ 0492.construct-the-rectangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def constructRectangle(self, area: int) -> List[int]:
+        _half = int(area ** 0.5)
+        for _l in range(_half, area+1):
+            _w = area//_l
+            if _l * _w == area:
+                if _w > _l:
+                    _l, _w = _w, _l
+                return [_l, _w]
+
