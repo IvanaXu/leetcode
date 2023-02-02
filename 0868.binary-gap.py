e6b00ed11e30a5f3063dfa4dbd724c01adf97c8e
IvanaXu <1440420407@qq.com>
2023-02-03 04:57:00 +0800

    Create|Update 0868.binary-gap.py

--- 0868.binary-gap.py
+++ 0868.binary-gap.py
@@ -1,3 +1,23 @@
+# Python2
+class Solution(object):
+    def binaryGap(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        _st, _max = -1, 0
+        for nb, vb in enumerate(bin(n).replace("0b","")):
+            if vb == "1":
+                if _st == -1:
+                    _st = nb
+                else:
+                    maxb = nb - _st
+                    if maxb > _max:
+                        _max = maxb
+                    _st = nb
+        return _max
+
+# Python3
 class Solution:
     def binaryGap(self, n: int) -> int:
         _st, _max = -1, 0
