abefe317a229ef9b7762eb0dd4a3d0489795a4a8
IvanaXu <1440420407@qq.com>
2023-02-02 10:21:58 +0800

    Create|Update 2011.final-value-of-variable-after-performing-operations.py

--- 2011.final-value-of-variable-after-performing-operations.py
+++ 2011.final-value-of-variable-after-performing-operations.py
@@ -2,8 +2,5 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         x = 0
         for o in operations:
-            if "+" in o:
-                x += 1
-            else:
-                x -= 1
+            x += 1 if "+" in o else -1
         return x
