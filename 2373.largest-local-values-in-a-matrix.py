81c6588bfcfc6b0886edba649ffd515d44d841ab
IvanaXu <1440420407@qq.com>
2023-02-02 13:59:45 +0800

    Create|Update 2373.largest-local-values-in-a-matrix.py

--- 2373.largest-local-values-in-a-matrix.py
+++ 2373.largest-local-values-in-a-matrix.py
@@ -1,5 +1,23 @@
+# Python2
 class Solution:
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        _n, N = len(grid)-2, 3
+        return [
+            [
+                max([
+                    max(i[n2: n2+N])
+                    for n, i in enumerate(grid)
+                    if n1 <= n < n1+N
+                ])
+                for n2 in range(_n)
+            ]
+            for n1 in range(_n)
+        ]
+
+# Python3 
+class Solution:
+    def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
+        # WARNNING: VAR n
         n, N = len(grid)-2, 3
         return [
             [
