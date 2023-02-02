8972ce9b3783e32c2d6ae5644af28ace4c7ab9d3
IvanaXu <1440420407@qq.com>
2023-02-02 08:53:30 +0800

    Create|Update 0507.perfect-number.py

--- 0507.perfect-number.py
+++ 0507.perfect-number.py
@@ -1,4 +1,7 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
+        # Python2
         # return num in [6, 28, 496, 8128, 33550336]
+        
+        # Python3 hash
         return num in {6, 28, 496, 8128, 33550336}
