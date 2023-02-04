b73aab4e5b23a451c2ca985fbf3e0e292d12903f
IvanaXu <1440420407@qq.com>
2023-02-04 10:18:23 +0800

    Create|Update 2451.odd-string-difference.py

---
+++ 2451.odd-string-difference.py
@@ -0,0 +1,18 @@
+c = {v: n for n, v in enumerate("abcdefghijklmnopqrstuvwxyz")}
+class Solution:
+    def oddString(self, words: List[str]) -> str:
+        r = {}
+        for w in words:
+            _v, _r = "", ""
+            for n, v in enumerate(w):
+                if n == 0:
+                    _v = v
+                else:
+                    _r += f"/{c.get(v,0) - c.get(_v,0)}"
+                    _v = v
+            # 
+            if _r not in r:
+                r[_r] = {"word": w, "cnts": 1}
+            else:
+                r[_r]["cnts"] += 1
+        return [_v["word"] for _k, _v in r.items() if _v["cnts"] == 1][0]
