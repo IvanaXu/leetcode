248b70a84d333bed83cb62b63731d28226b93e9b
IvanaXu <1440420407@qq.com>
2023-02-05 15:42:57 +0800

    Create|Update 0013.roman-to-integer.py

--- 0013.roman-to-integer.py
+++ 0013.roman-to-integer.py
@@ -1,55 +1,27 @@
+_c = [
+    ["M",   1000],
+    ["D",   500],
+    ["CD",  400],
+    ["CM",  900],
+    ["C",   100],
+    ["L",   50],
+    ["XL",  40],
+    ["XC",  90],
+    ["X",   10],
+    ["V",   5],
+    ["IV",  4],
+    ["IX",  9],
+    ["III", 3],
+    ["II",  2],
+    ["I",   1],
+]
 class Solution:
     def romanToInt(self, s: str) -> int:
         _r = 0
         while s:
-            if s.startswith("M"):
-                s = s[1:]
-                _r += 1000
-            elif s.startswith("D"):
-                s = s[1:]
-                _r += 500
-            # 
-            elif s.startswith("CD"):
-                s = s[2:]
-                _r += 400
-            elif s.startswith("CM"):
-                s = s[2:]
-                _r += 900
-            elif s.startswith("C"):
-                s = s[1:]
-                _r += 100
-            #
-            elif s.startswith("L"):
-                s = s[1:]
-                _r += 50
-            # 
-            elif s.startswith("XL"):
-                s = s[2:]
-                _r += 40
-            elif s.startswith("XC"):
-                s = s[2:]
-                _r += 90
-            elif s.startswith("X"):
-                s = s[1:]
-                _r += 10
-            # 
-            elif s.startswith("V"):
-                s = s[1:]
-                _r += 5
-            # 
-            elif s.startswith("IV"):
-                s = s[2:]
-                _r += 4
-            elif s.startswith("IX"):
-                s = s[2:]
-                _r += 9
-            elif s.startswith("III"):
-                s = s[3:]
-                _r += 3
-            elif s.startswith("II"):
-                s = s[2:]
-                _r += 2
-            elif s.startswith("I"):
-                s = s[1:]
-                _r += 1
+            for [_k, _v] in _c:
+                if s.startswith(_k):
+                    s = s[len(_k):]
+                    _r += _v
+                    break
         return _r
