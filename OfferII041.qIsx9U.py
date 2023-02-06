5676c50fa4d017a17e15c54e9f482d96f1a3aa60
IvanaXu <1440420407@qq.com>
2023-02-07 07:26:20 +0800

    Create|Update OfferII041.qIsx9U.py

---
+++ OfferII041.qIsx9U.py
@@ -0,0 +1,21 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        """
+        Initialize your data structure here.
+        """
+        self.size = size
+        self.nums = []
+
+
+    def next(self, val: int) -> float:
+        if len(self.nums) >= self.size:
+            self.nums.pop(0)
+        self.nums.append(val)
+        return mean(self.nums)
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
