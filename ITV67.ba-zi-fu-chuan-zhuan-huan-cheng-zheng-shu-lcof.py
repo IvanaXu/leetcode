7546cbefe735e221f29c9358203f9a687c588d73
IvanaXu <1440420407@qq.com>
2023-02-06 17:28:01 +0800

    Create|Update ITV67.ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof.py

---
+++ ITV67.ba-zi-fu-chuan-zhuan-huan-cheng-zheng-shu-lcof.py
@@ -0,0 +1,24 @@
+c1 = set("-+")
+c2 = set("0123456789")
+_min, _max = -2147483648, 2147483647
+class Solution:
+    def strToInt(self, s: str) -> int:
+        _r = ""
+        for i in s:
+            if not _r:
+                if i == " ":
+                    pass
+                elif i in c1|c2:
+                    _r += i
+                else:
+                    break
+            else:
+                if i in c2:
+                    _r += i
+                else:
+                    break
+        try:
+            _rint = int(_r)
+        except:
+            _rint = 0
+        return min([max([_rint, _min]), _max])
