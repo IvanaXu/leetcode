00f212812f756132e57370f82c16dc570e7b6913
IvanaXu <1440420407@qq.com>
2023-02-08 07:06:00 +0800

    Delete 6348.take-gifts-from-the-richest-pile.py

--- 6348.take-gifts-from-the-richest-pile.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def pickGifts(self, gifts: List[int], k: int) -> int:
-        gifts = sorted(gifts)
-        for _ in range(k):
-            gifts = sorted(gifts)
-            gifts[-1] = int(math.sqrt(gifts[-1]))
-        return sum(gifts)
