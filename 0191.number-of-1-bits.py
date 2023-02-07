d85685ae245c60b00d344ee3ae3929093ea1e01c
IvanaXu <1440420407@qq.com>
2023-02-07 17:07:25 +0800

    Create|Update 0191.number-of-1-bits.py

--- 0191.number-of-1-bits.py
+++ 0191.number-of-1-bits.py
@@ -1,3 +1,13 @@
+# Python2
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return bin(n).count("1")
+
+# Python3
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return bin(n).count("1")
+        return f"{n:b}".count("1")
