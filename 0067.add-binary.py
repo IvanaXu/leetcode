b88479d906a317fdae861bdc554621ead31d7a19
IvanaXu <1440420407@qq.com>
2023-02-07 08:33:34 +0800

    Create|Update 0067.add-binary.py

--- 0067.add-binary.py
+++ 0067.add-binary.py
@@ -1,3 +1,14 @@
+# Python2
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int(a, 2)+int(b, 2))[2:]
+
+# Python3
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return f"{int(a, 2) + int(b, 2):b}"
