6a75e364b0dae70341e79d037f0dd4b68e393dc6
IvanaXu <1440420407@qq.com>
2023-02-07 08:37:27 +0800

    Create|Update 0029.divide-two-integers.py

--- 0029.divide-two-integers.py
+++ 0029.divide-two-integers.py
@@ -1,3 +1,14 @@
+# Python2
+class Solution(object):
+    def divide(self, dividend, divisor):
+        """
+        :type dividend: int
+        :type divisor: int
+        :rtype: int
+        """
+        return min([2147483647, int(dividend/float(divisor))])
+
+# Python3
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
         return min([2147483647, int(dividend/divisor)])
