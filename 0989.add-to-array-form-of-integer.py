ecf182e2659af2a0f5e6cbe9e49134330c1d0e83
IvanaXu <1440420407@qq.com>
2023-02-08 21:55:24 +0800

    Create|Update 0989.add-to-array-form-of-integer.py

--- 0989.add-to-array-form-of-integer.py
+++ 0989.add-to-array-form-of-integer.py
@@ -1,3 +1,4 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        return [int(i) for i in str(int("".join([str(i) for i in num]))+k)]
+        # return [int(i) for i in str(int("".join([str(i) for i in num]))+k)]
+        return list(map(int, str(int("".join(map(str, num)))+k)))
