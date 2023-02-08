03950bd355b61d63ccc2b3500d4c3839d1f22106
IvanaXu <1440420407@qq.com>
2023-02-08 21:45:26 +0800

    Create|Update ITV01.06.compress-string-lcci.py

--- ITV01.06.compress-string-lcci.py
+++ ITV01.06.compress-string-lcci.py
@@ -1,3 +1,21 @@
+# Python2
+class Solution(object):
+    def compressString(self, S):
+        """
+        :type S: str
+        :rtype: str
+        """
+        _r = ""
+        _st, _sn = "", 0
+        for s in S+"/":
+            if s != _st:
+                _r += "{0}{1}".format(_st,_sn) if _sn > 0 else ""
+                _st, _sn = s, 1
+            else:
+                _sn += 1
+        return _r if len(_r) < len(S) else S
+
+# Python3
 class Solution:
     def compressString(self, S: str) -> str:
         _r = ""
