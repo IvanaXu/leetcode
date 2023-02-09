616f9bc271c37b74abf01d63998894321755e171
IvanaXu <1440420407@qq.com>
2023-02-10 03:57:02 +0800

    Create|Update 0179.largest-number.py

--- 0179.largest-number.py
+++ 0179.largest-number.py
@@ -1,8 +1,7 @@
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        _r = "".join(
-            sorted(map(str, nums),
+        return str(int(''.join(sorted(
+            map(str, nums),
             key=lambda x: x * (100 if len(x) == 1 else 2),
             reverse=True
-        ))
-        return f"{int(_r)}"
+        ))))
