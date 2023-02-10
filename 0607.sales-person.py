d3d24a9037b606e497ca6842353bda43fe45e508
IvanaXu <1440420407@qq.com>
2023-02-10 09:37:58 +0800

    Create|Update 0607.sales-person.py

---
+++ 0607.sales-person.py
@@ -0,0 +1,16 @@
+SQL = """
+# Write your MySQL query statement below
+SELECT 
+    name
+FROM SalesPerson
+WHERE sales_id NOT IN (
+    SELECT 
+        DISTINCT sales_id 
+    FROM Orders
+    WHERE com_id IN (
+        SELECT com_id
+        FROM Company
+        WHERE name = "RED"
+    )
+)
+"""
