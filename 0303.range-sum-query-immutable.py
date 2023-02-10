eac91c9cd086ef6cb2d77e417c729450031edc28
IvanaXu <1440420407@qq.com>
2023-02-10 19:22:40 +0800

    Create|Update 0303.range-sum-query-immutable.py

--- 0303.range-sum-query-immutable.py
+++ 0303.range-sum-query-immutable.py
@@ -1,11 +1,12 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.q = nums
-
+        self.q = {0: 0}
+        for n, v in enumerate(nums):
+            self.q[n+1] = self.q[n] + v
+    
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.q[left: right+1])
-
+        return self.q.get(right+1, 0) - self.q.get(left, 0)
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
