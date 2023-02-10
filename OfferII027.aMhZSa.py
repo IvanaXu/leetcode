e8872d7921c31095fe30c4cbda9696e0642338a6
IvanaXu <1440420407@qq.com>
2023-02-11 03:17:21 +0800

    Create|Update OfferII027.aMhZSa.py

---
+++ OfferII027.aMhZSa.py
@@ -0,0 +1,12 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        _r = []
+        while head:
+            _r.append(head.val)
+            head = head.next
+        return _r == _r[::-1]
