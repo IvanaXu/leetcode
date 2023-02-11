4e45b8cfaaef6b0034fe124f5cc42e00abfd09c2
IvanaXu <1440420407@qq.com>
2023-02-11 08:55:13 +0800

    Create|Update 0021.merge-two-sorted-lists.py

--- 0021.merge-two-sorted-lists.py
+++ 0021.merge-two-sorted-lists.py
@@ -3,19 +3,20 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+import bisect
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        tl1, tl2 = [], []
+        tl = []
         while list1:
-            tl1.append(list1.val)
+            bisect.insort(tl, list1.val)
             list1 = list1.next
         while list2:
-            tl2.append(list2.val)
+            bisect.insort(tl, list2.val)
             list2 = list2.next
         
         _r = ListNode(0)
         t = _r
-        for i in sorted(tl1 + tl2):
+        for i in tl:
             t.next = ListNode(i)
             t = t.next
         return _r.next
