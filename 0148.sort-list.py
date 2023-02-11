0f242c5d4ed694d6254002eb1af7ff06c2f3f801
IvanaXu <1440420407@qq.com>
2023-02-11 15:59:48 +0800

    Create|Update 0148.sort-list.py

---
+++ 0148.sort-list.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        import bisect
+        _v = []
+        while head:
+            bisect.insort(_v, head.val)
+            head = head.next
+
+        _r = ListNode(0)
+        t = _r
+        for v in _v:
+            t.next = ListNode(v)
+            t = t.next
+        return _r.next
