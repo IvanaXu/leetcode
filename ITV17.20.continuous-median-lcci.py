49ae99e8b33f3b0b6b1380408464d511e97e5a30
IvanaXu <1440420407@qq.com>
2023-02-12 21:03:13 +0800

    Create|Update ITV17.20.continuous-median-lcci.py

---
+++ ITV17.20.continuous-median-lcci.py
@@ -0,0 +1,26 @@
+class MedianFinder:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        import bisect
+        self.n = 0
+        self.l, self.r = 0, 0
+        self.s = []
+
+    def addNum(self, num: int) -> None:
+        self.n += 1
+        self.l = self.n//2-1 if self.n%2 == 0 else self.n//2
+        self.r = self.n//2
+        bisect.insort(self.s, num)
+
+
+    def findMedian(self) -> float:
+        return (self.s[self.l] + self.s[self.r])/2.0
+
+
+# Your MedianFinder object will be instantiated and called as such:
+# obj = MedianFinder()
+# obj.addNum(num)
+# param_2 = obj.findMedian()
