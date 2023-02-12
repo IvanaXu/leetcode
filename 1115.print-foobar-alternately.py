3f95955d15ae6d8af690cff9e4004c7869add430
IvanaXu <1440420407@qq.com>
2023-02-12 11:35:43 +0800

    Create|Update 1115.print-foobar-alternately.py

--- 1115.print-foobar-alternately.py
+++ 1115.print-foobar-alternately.py
@@ -1,3 +1,33 @@
+# Python2
+import threading
+class FooBar(object):
+    def __init__(self, n):
+        self.n = n
+        self.lock_foo = threading.Lock()
+        self.lock_bar = threading.Lock()
+        self.lock_bar.acquire() #
+
+    def foo(self, printFoo):
+        """
+        :type printFoo: method
+        :rtype: void
+        """
+        for i in xrange(self.n):
+            self.lock_foo.acquire()
+            printFoo()
+            self.lock_bar.release()
+
+    def bar(self, printBar):
+        """
+        :type printBar: method
+        :rtype: void
+        """
+        for i in xrange(self.n):
+            self.lock_bar.acquire()
+            printBar()
+            self.lock_foo.release()
+
+# Python3
 import threading
 class FooBar:
     def __init__(self, n):
