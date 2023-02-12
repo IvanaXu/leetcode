d7259b3513cef682f6c4a090b3fe5019f0df6a53
IvanaXu <1440420407@qq.com>
2023-02-12 21:24:28 +0800

    Create|Update ITV17.19.missing-two-lcci.py

--- ITV17.19.missing-two-lcci.py
+++ ITV17.19.missing-two-lcci.py
@@ -1,10 +1,24 @@
+#
 class Solution:
     def missingTwo(self, nums: List[int]) -> List[int]:
         _r, _n, _l = [], 0, len(nums)
-        # _nums = set(nums)
         for i in range(1, _l+2+1):
             if i in nums:
                 nums.remove(i)
+            else:
+                _n += 1
+                _r.append(i)
+            if _n == 2:
+                return _r
+
+#
+class Solution:
+    def missingTwo(self, nums: List[int]) -> List[int]:
+        _nums = set(nums)
+        _r, _n, _l = [], 0, len(_nums)
+        for i in range(1, _l+2+1):
+            if i in _nums:
+                _nums.remove(i)
             else:
                 _n += 1
                 _r.append(i)
