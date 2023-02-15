56a2b0d174543da492600936204b0aa6eeb4b50d
IvanaXu <1440420407@qq.com>
2023-02-15 21:30:25 +0800

    Create|Update 1619.mean-of-array-after-removing-some-elements.py

--- 1619.mean-of-array-after-removing-some-elements.py
+++ 1619.mean-of-array-after-removing-some-elements.py
@@ -1,3 +1,15 @@
+# Python2
+class Solution(object):
+    def trimMean(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: float
+        """
+        _l = len(arr)
+        arr.sort()
+        return sum(arr[_l//20:_l-_l//20])/(_l//20*18.0)
+
+# Python3
 class Solution:
     def trimMean(self, arr: List[int]) -> float:
         _l = len(arr)
